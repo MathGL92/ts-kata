a2e0c7f9f624aa5efe50d87b20f4e4150d265810
Mathieu Longe <mathieu.longe@orange.fr>
2023-10-26 12:19:43 +1100

    chore: complete fizzbuzz kata

--- src/1_FizzBuzz/kata.ts
+++ src/1_FizzBuzz/kata.ts
@@ -1,1 +1,16 @@
-export const fizzBuzz = 'something';
+export const fizzBuzz = (input: number) => {
+  if (isMultipleOf(3, input) && isMultipleOf(5, input)) {
+    return 'fizzbuzz';
+  }
+  if (isMultipleOf(3, input)) {
+    return 'fizz';
+  }
+  if (isMultipleOf(5, input)) {
+    return 'buzz';
+  }
+  return input.toString();
+};
+
+const isMultipleOf = (multiple: number, input: number): boolean => {
+  return input % multiple === 0;
+};
--- src/1_FizzBuzz/test.ts
+++ src/1_FizzBuzz/test.ts
@@ -1,7 +1,39 @@
 import { fizzBuzz } from './kata';
 
 describe('Fizz Buzz should', () => {
-  test('be "something"', () => {
-    expect(fizzBuzz).toBe('something');
+  test('when input is 1 output is 1', () => {
+    expect(fizzBuzz(1)).toBe('1');
+  });
+
+  test('when input is 2 output is 2', () => {
+    expect(fizzBuzz(2)).toBe('2');
+  });
+
+  test('when input is 3 output is fizz', () => {
+    expect(fizzBuzz(3)).toBe('fizz');
+  });
+
+  test('when input is 6 output is fizz', () => {
+    expect(fizzBuzz(6)).toBe('fizz');
+  });
+
+  test('when input is 9 output is fizz', () => {
+    expect(fizzBuzz(9)).toBe('fizz');
+  });
+
+  test('when input is 5 output is buzz', () => {
+    expect(fizzBuzz(5)).toBe('buzz');
+  });
+
+  test('when input is 10 output is buzz', () => {
+    expect(fizzBuzz(10)).toBe('buzz');
+  });
+
+  test('when input is 20 output is buzz', () => {
+    expect(fizzBuzz(20)).toBe('buzz');
+  });
+
+  test('when input is 15 output is fizzbuzz', () => {
+    expect(fizzBuzz(15)).toBe('fizzbuzz');
   });
 });
