c2d4051e4283e503c73fe134fbe6b8fd853c2212
Mathieu Longe <mathieu.longe@orange.fr>
2023-11-08 20:17:11 +1100

    feat: complete leap kata

--- src/2_LeapYear/kata.ts
+++ src/2_LeapYear/kata.ts
@@ -1,1 +1,9 @@
-export const leapYear = true;
+export const leapYear = (year: number): boolean => {
+  if (year % 400 === 0) {
+    return true;
+  }
+  if (year % 100 === 0) {
+    return false;
+  }
+  return year % 4 === 0;
+};
--- src/2_LeapYear/test.ts
+++ src/2_LeapYear/test.ts
@@ -1,7 +1,23 @@
 import { leapYear } from './kata';
 
 describe('Leap year should', () => {
-  test('be allways', () => {
-    expect(leapYear).toBe(true);
+  test('return true if input is 4', () => {
+    expect(leapYear(4)).toBe(true);
+  });
+
+  test('return true if input is 8', () => {
+    expect(leapYear(8)).toBe(true);
+  });
+
+  test('return false if input is 1', () => {
+    expect(leapYear(1)).toBe(false);
+  });
+
+  test('return false if input is 100', () => {
+    expect(leapYear(100)).toBe(false);
+  });
+
+  test('return true if input is 400', () => {
+    expect(leapYear(400)).toBe(true);
   });
 });
